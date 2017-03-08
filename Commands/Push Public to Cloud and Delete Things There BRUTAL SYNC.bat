cba61037f4e1d6def38109cb98fe6c38bc423e62
MichaelCook <michael@evoluted.net>
2017-03-08 22:59:24 +0000

    extra sec

--- Commands/Push Public to Cloud and Delete Things There BRUTAL SYNC.bat
+++ Commands/Push Public to Cloud and Delete Things There BRUTAL SYNC.bat
@@ -1,10 +1,14 @@
 @echo off
 echo.
-echo This is called 'sync' but is brutal - it actually only modifies the cloud, copying files from local to the cloud AND deleting files when they don't exist locally.
+echo This is called 'sync' but is brutal - it only modifies the cloud, copying files from local to the cloud AND deleting files there when they don't exist locally.
 echo.
 echo It DOESN'T download files.
 echo.
 echo Are you sure?
+echo.
+pause
+echo.
+echo Are you REALLY sure? I mean, you probably never need this.
 echo.
 pause
 echo rclone sync ../../Public amazon:Public
